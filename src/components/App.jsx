2c8d5d94a9ac6f5d80749658b7e6cad1cbe113b9
Андрій <74345435+Andriy-Lesyk@users.noreply.github.com>
2022-05-24 09:55:05 +0300

    виправляє помилку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,7 @@ import Filter from './Filter/Filter';
 import Contact from './Contacts/Contacts';
 
 export default function App() {
-   function a(x,y){
-    x=3
-    y=5 
-    let c=x+y
-    console.log(c)
-
-}
+ 
   console.log(arguments)
   return (
     <div>
